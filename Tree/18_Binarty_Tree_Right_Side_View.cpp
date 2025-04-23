eaeb49d9f70bcf5918113b6ea2b508ade935a761
shivam <shivam.singhal.211@gmail.com>
2025-04-23 22:55:24 +0530

    Right/Left Side View of BT

---
+++ Tree/18_Binarty_Tree_Right_Side_View.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public:
+        void help(TreeNode* root,int level,vector<int>& ans)
+        {
+            if(root==NULL)return;
+            if(level==ans.size())ans.push_back(root->val);
+            if(root->right)help(root->right,level+1,ans);
+            if(root->left)help(root->left,level+1,ans);
+        }
+        vector<int> rightSideView(TreeNode* root) {
+            vector<int> ans;
+            help(root,0,ans);
+            return ans;
+        }
+    };
